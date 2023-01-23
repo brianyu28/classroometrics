34a98ca3e388dfb5d7aa3dcc51f2d2c0ca7f727c
Brian Yu <brian@brianyu.me>
2023-01-22 20:41:06 -0500

    Redirect short URLs to viewer app

--- classroometrics/urls.py
+++ classroometrics/urls.py
@@ -3,10 +3,14 @@ Root URL configuration.
 """
 
 from django.contrib import admin
+from django.http.request import HttpRequest
 from django.shortcuts import redirect
 from django.urls import include, path
 
 from . import views
+
+def redirect_to_viewer(request: HttpRequest, identifier: str):
+    return redirect("viewer", identifier=identifier)
 
 urlpatterns = [
     path("", lambda request: redirect("/app/")),
@@ -14,4 +18,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("app/", include("core.urls")),
+    path("<str:identifier>", redirect_to_viewer)
 ]
